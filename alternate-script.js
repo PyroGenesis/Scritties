80e313f959394aa68cfa81ba4fc814801cf9f556
Burhanuddin Mustsfa Lakdawala <burhan123786@gmail.com>
2021-10-17 16:44:03 -0700

    added observe and few auto conversions for resource cap in alternate script

--- alternate-script.js
+++ alternate-script.js
@@ -1,7 +1,13 @@
 import { SCRITTIES_LOG } from "./config/log";
 import { SCRITTIES_SETTINGS } from "./config/settings";
+import { resourceToResourceMapping } from "./ref/res-to-res";
 import { farm } from "./scripts/actions/farm";
 import { hunt } from "./scripts/actions/hunt";
+import { observe } from "./scripts/actions/observe";
+import { useUpResources } from "./scripts/use-resources/general";
+
+SCRITTIES_LOG.observe = true;
+let observeInterval = setInterval(observe, 2000);
 
 SCRITTIES_LOG.farm = true;
 SCRITTIES_SETTINGS.farm = true;
@@ -12,3 +18,12 @@ SCRITTIES_SETTINGS.catpower.hunt = true;
 SCRITTIES_LOG.catpower.parchment = false;
 SCRITTIES_SETTINGS.catpower.parchment = false;
 let huntInterval = setInterval(hunt, 5000);
+
+SCRITTIES_LOG.crafting.capPrevention = true;
+resourceToResourceMapping.splice(0, 1);
+resourceToResourceMapping.splice(4);
+let useResourcesInterval = setInterval(() => {
+    // builder();
+    // minerals();
+    useUpResources();
+}, 1 * 1000);
