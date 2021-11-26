487272aac73bd7671ca69ca59fb86e95ca98e0cd
Burhanuddin Mustsfa Lakdawala <burhan123786@gmail.com>
2021-11-26 14:59:55 -0800

    alternate-script updated

--- alternate-script.js
+++ alternate-script.js
@@ -1,29 +1,35 @@
-import { SCRITTIES_LOG } from "./config/log";
 import { SCRITTIES_SETTINGS } from "./config/settings";
 import { resourceToResourceMapping } from "./ref/res-to-res";
 import { farm } from "./scripts/actions/farm";
 import { hunt } from "./scripts/actions/hunt";
 import { observe } from "./scripts/actions/observe";
 import { useUpResources } from "./scripts/use-resources/general";
+import { upgrade } from "./scripts/actions/upgrade";
+import { constructionAutoUpgrades } from "./ref/upgrade-mapping";
 
-SCRITTIES_LOG.observe = true;
-let observeInterval = setInterval(observe, 2000);
 
-SCRITTIES_LOG.farm = true;
+// SCRITTIES_LOG.catpower.hunt = true;
+// SCRITTIES_LOG.catpower.parchment = true;
+let huntInterval = setInterval(hunt, 5000);
+
+// SCRITTIES_LOG.farm = true;
 SCRITTIES_SETTINGS.farm = true;
 let farmInterval = setInterval(farm, 2000);
 
-SCRITTIES_LOG.catpower.hunt = true;
-SCRITTIES_SETTINGS.catpower.hunt = true;
-SCRITTIES_LOG.catpower.parchment = false;
-SCRITTIES_SETTINGS.catpower.parchment = false;
-let huntInterval = setInterval(hunt, 5000);
+// SCRITTIES_LOG.observe = true;
+let observeInterval = null;
+if (!game.workshop.get('seti').researched) {
+    observeInterval = setInterval(observe, 2000);
+}
 
-SCRITTIES_LOG.crafting.capPrevention = true;
-resourceToResourceMapping.splice(0, 1);
-resourceToResourceMapping.splice(4);
+resourceToResourceMapping.splice(5);
 let useResourcesInterval = setInterval(() => {
-    // builder();
-    // minerals();
+    // builder(game.bldTab, cathBuildHierarchy, 'CATH_BUILD_LastGroupReached');
+    // builder(game.spaceTab, spaceBuildHierarchy, 'SPACE_BUILD_LastGroupReached');
     useUpResources();
+    // gold(1 * 1000);
 }, 1 * 1000);
+
+constructionAutoUpgrades.splice(0, 3);
+constructionAutoUpgrades.splice(5);
+let upgradeInterval = setInterval(upgrade, 2 * 1000);
